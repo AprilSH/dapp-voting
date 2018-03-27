b23e82442a23193dacecfd347f27f2430a401f6e
AprilSH <aprilsh08@gmail.com>
2018-03-27 09:08:24 +0800

    fixed minor syntax errors

--- test/election.js
+++ test/election.js
@@ -14,18 +14,18 @@ contract("Election", function(accounts){
 
     //test if candidates are initialized with correct value
     it("it initializes the candidates with the correct values", function(){
-        return Election.deployed().then(function(instance))
+        return Election.deployed().then(function(instance){
         electionInstance = instance;
         return electionInstance.candidates(1);
-    }).then(function(candidate){
-        assert.equal(candidate[0], 1, "contains the correct id");
-        assert.equal(candidate[1], "Candidate 1", "contains the correct name");
-        assert.equal(candidate[2], 0, "contains the correct count of votes");
-        return electionInstance.candidates(2);
-    }).then(function(candidate){
-        assert.equal(candidate[0], 2, "contains the correct id");
-        assert.equal(candidate[1], "Candidate 2", "contains the correct name");
-        assert.equal(candidate[2], 0, "contains the correct count of votes");
+        }).then(function(candidate){
+            assert.equal(candidate[0], 1, "contains the correct id");
+            assert.equal(candidate[1], "Candidate 1", "contains the correct name");
+            assert.equal(candidate[2], 0, "contains the correct count of votes");
+            return electionInstance.candidates(2);
+        }).then(function(candidate){
+            assert.equal(candidate[0], 2, "contains the correct id");
+            assert.equal(candidate[1], "Candidate 2", "contains the correct name");
+            assert.equal(candidate[2], 0, "contains the correct count of votes");
+        });
     });
-
 });
